0ca1276e2bd822c41dfa68bb7ad734373f83c2b5
SanchezNoble <sancheznobs1@southernct.edu>
2024-04-11 11:46:46 -0400

    initial setup of forms.py and models.py

--- app/forms.py
+++ app/forms.py
@@ -3,14 +3,17 @@ from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 class AddForm(FlaskForm):
-    city = StringField('City:', validators=[DataRequired()])
-    population = IntegerField('Population: ', validators=[DataRequired()])
+    user_id = StringField('User ID:', validators=[DataRequired()])
+    phone_number = IntegerField('Phone Number: ', validators=[DataRequired()])
+    email_address = IntegerField('Email Address: ', validators=[DataRequired()])
+    password = IntegerField('Password: ', validators=[DataRequired()])
+    favorite_genre = IntegerField('Favorite Genre: ', validators=[DataRequired()])
     submit = SubmitField('Save')
-
+ 
 class DeleteForm(FlaskForm):
-    city = StringField('City:', validators=[DataRequired()])
+    user_id = StringField('User ID:', validators=[DataRequired()])
     submit = SubmitField('Delete')
 
 class SearchForm(FlaskForm):
-    city = StringField('City:', validators=[DataRequired()])
+    user_id = StringField('User ID:', validators=[DataRequired()])
     submit = SubmitField('Search')
--- app/models.py
+++ app/models.py
@@ -1,11 +1,21 @@
 from app import db
 
-# Define the DB schema
-class City(db.Model):
-    __tablename__ = 'cities'
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(64), unique=True, nullable=False)
-    population = db.Column(db.Integer, unique=False, nullable=False)
+# Database Classes
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column(db.String(64), primary_key=True)
+    phone_number= db.Column(db.String(64), unique=False, nullable=False)
+    email_address = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), unique=False, nullable=False)
+    favorite_genre = db.Column(db.String(64), unique=False, nullable=False)
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    admin_id = db.Column(db.String(64), primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    phone_number= db.Column(db.String(64), unique=False, nullable=False)
+    email_address = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), unique=False, nullable=False)
 
     def __repr__(self):
-        return self.city + ': ' + str(self.population)
+        return self.user_id + ': ' + str(self.email_address)
