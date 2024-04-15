510940492fbc9f7ee763c56c7e1b519b9d07d6cd
evanmgutowski <evanmgutowski@gmail.com>
2024-04-15 03:21:19 +0000

    spelling mistakes

--- app/register.py
+++ app/register.py
@@ -4,17 +4,17 @@ conn = sqlite3.connect('profiles.db')
 
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS user
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
                 (email_address TEXT NOT NULL,
                 password NOT NULL)''')
 
 def register_user(email, user_password):
     cursor.execute('SELECT * FROM users WHERE email_address=?', email)
-    user = cursor.fecthone()
+    user = cursor.fetchone()
     if user:
         return 'An account using this email address already exists'
     else:
         cursor.execute('INSERT INTO user (email_address, password) VALUES (?, ?)',(email, user_password))
-
-conn.commit()
+        conn.commit
+        
 conn.close()
