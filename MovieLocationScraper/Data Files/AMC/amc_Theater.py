053123a4f4572136605f776c2e033b5d349c0993
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2024-04-24 10:08:14 -0400

    got it to scrape data, now woorking on filters

--- MovieLocationScraper/Data Files/AMC/amc_Theater.py
+++ MovieLocationScraper/Data Files/AMC/amc_Theater.py
@@ -5,28 +5,25 @@ import datetime
 f1 = open("MovieLocationScraper/Data Files/AMC/AMC-Theaters-List.txt", "r")
 f2 = open("C:/Users/mikec/Desktop/Github/CSC330-TheaterHub/MovieLocationScraper/Data Files/AMC/AMC-Theaters-HTML.txt", "w")
 date = datetime.datetime.now()
+
+
+# print(date)
 i = 5
 
-test = [str(f1.readline()).strip().split("/")]
-
-print(test)
+location = str(f1.readline()).strip().split("/")
 
 
 # soup = BeautifulSoup(f1.readline(), "html.parser")
 
-# print(f1.readline() + "/showtimes/all/" + str(date.year) + "-" + str(date.month) + "-" + str(date.day))
+for x in range(1):
+    OURL = f1.readline().strip()
+    NURL = OURL + "/showtimes/all/" + str(date.year) + "-" + str(date.month) + "-" + str(date.day)
 
-# for x in range(5):
-#     URL = f1.readline() + "/showtimes/all/" + str(date.year) + "-" + str(date.month) + "-" + str(date.day) + 
-#     print(URL)
+    print(NURL)
 
-#     page = requests.get(URL)
-
-#     soup = BeautifulSoup(page.content, "html.parser")
-#     f2.write(str(soup))
-#     colInfo = soup.find_all("div")
-
-#     #f2.write("\n\n\n\n" + URL)
+    page = requests.get(NURL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    f2.write( str(NURL) + "\n\n" + str(soup) + "\n\n\n")
 
 
-# # print(set([tag.name for tag in soup.find_all()]))
