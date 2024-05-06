53520d88eac68a5b2875857a6eff899c06ebc6f7
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2024-05-06 00:11:57 -0400

    final edits

--- MovieLocationScraper/Data Files/AMC/AMC_Scraper_Selenium.py
+++ MovieLocationScraper/Data Files/AMC/AMC_Scraper_Selenium.py
@@ -10,17 +10,22 @@ import schedule
 def job():
     f1 = open("MovieLocationScraper/Data Files/AMC/AMC-Theaters-List.txt", "r")
 
+    #Creates a headless webdriver to mimic a human using Selenium
     options = Options()
     options.add_argument("-headless")
     driver = webdriver.Firefox(options=options)
     movie_list = []
 
+    #filtering through the 557 links for each theater from "AMC-Theaters-List.txt"
     for x in range(557):
         ourl = f1.readline().strip()
-        url = ourl + "/showtimes"
-        # print(url)
-        driver.get(url)
-        print("Headless Firefox Initialized for URL: " + url)
+
+        # Adding /showtimes to display all movies playing at this location. otherwise it would pull the main menu for that theater.
+        # then the webdriver gets the url of the theater and parses the HTML of each web page using BeautifulSoup. 
+        #program sleeps for 3 seconds to avoid being IP blocked.
+        url = ourl + "/showtimes"   
+        driver.get(url)     
+        print("Headless Firefox Initialized for URL: " + url) 
         time.sleep(3)
         location = url.strip('/').split('/')[-2]
         theater = url.strip('/').split('/')[-3]
@@ -28,7 +33,9 @@ def job():
         soup = BeautifulSoup(html, "html.parser")
         all_data = soup.find_all(['div'], class_='ShowtimesByTheatre-film')
 
-        
+        #filtering through each h2 element and getting the text.
+        #then it finds all buttons with the class name below.
+        #finally it appends the data from the button and adds it to the showtimes list
         for x in all_data:
 
             showtimes = []
@@ -41,7 +48,8 @@ def job():
             st_unit = x.find_all("a", class_="Btn Btn--default")
             for st in st_unit:
                 showtimes.append(st.get_text())
-
+            
+            #this creates a movie with its respective details and adds it to the movie_list
             for t in showtimes:
                 mov_dat = {
                     'movie': title,
@@ -51,16 +59,19 @@ def job():
                 }
                 movie_list.append(mov_dat)
 
-
+    #opens our amc-movies-json file so we can write to it
     with open('MovieLocationScraper\Data Files\AMC\\amc-movies.json', 'w') as file:
-        json.dump(movie_list, file, indent=4)
+        json.dump(movie_list, file, indent=4) #dumps our movie list into the json file
 
-    driver.quit()
+    print("Done!")
+    driver.quit() #stops the web driver to remove any unnecessary resources
 
 job()
 
-schedule.every().day.at("00:00").do(job)
+#schedule to run the scraper every day at midnight
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# schedule.every().day.at("00:00").do(job)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
